844372caafc3e6e3bb3508bc2d5115566280b45e
pkiop <pkiopb@gmail.com>
2022-01-11 15:19:17 +0900

    playground: Input 컴포넌트 테스트 추가

--- playground/src/views/home/layout.tsx
+++ playground/src/views/home/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import InputList from '../../components/input-list';
 
 const HomeLayout = () => {
   return (
     <div>
       <h1>home</h1>
+      <InputList />
     </div>
   );
 };
