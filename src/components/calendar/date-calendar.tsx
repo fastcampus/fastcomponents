ad751b6eda32e44d529dc27f0d4c0ca9e48774c5
pkiop <pkiopb@gmail.com>
2022-01-13 10:56:53 +0900

    하위 class에 프리픽스 제거

--- src/components/calendar/date-calendar.tsx
+++ src/components/calendar/date-calendar.tsx
@@ -5,9 +5,7 @@ import type { DateCalendarProps } from 'src/types/calendar.interface';
 const DateCalendar = ({ dateNumber }: DateCalendarProps) => {
   const { selectedDate, isCurrentYearMonth } = useContext(CalendarContext);
   return (
-    <div
-      className={`fc-date-calendar ${selectedDate.getDate() === dateNumber && isCurrentYearMonth ? 'selected' : ''} `}
-    >
+    <div className={`date-calendar ${selectedDate.getDate() === dateNumber && isCurrentYearMonth ? 'selected' : ''} `}>
       {dateNumber <= 0 ? '' : dateNumber}
     </div>
   );
