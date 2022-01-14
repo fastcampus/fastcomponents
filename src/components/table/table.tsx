cd08063d2f0bac2fa8d5cab8c41e89c01df67ba8
pkiop <pkiopb@gmail.com>
2022-01-14 10:35:20 +0900

    table: props로 마크업 받도록 수정

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import type { TableProps } from '../../types/table.interface';
 
-const Table = ({ children }: TableProps) => {
-  if (!children) {
-    throw new Error('insert children!');
-  }
-
+const Table = ({ head, body }: TableProps) => {
   return (
     <table className="fc-table">
       <thead>
-        <tr>{children[0]}</tr>
+        <tr>{head}</tr>
       </thead>
-      <tbody>{children[1]}</tbody>
+      <tbody>{body}</tbody>
     </table>
   );
 };
