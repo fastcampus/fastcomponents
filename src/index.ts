54e74a2a5ca41ee0b4a51dcd2db4cb0e041c8d09
pkiop <pkiopb@gmail.com>
2022-01-17 09:51:51 +0900

    pagination: Sort 기능 제거

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import Input from './components/input';
 import Calendar from './components/calendar';
 import Table from './components/table';
 import Pagination from './components/pagination';
-export type { Sort } from './types/pagination.interface';
 
 export { Input, Calendar, Table, Pagination };
