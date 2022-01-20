67f528dceb403dc0191842cff85701cd344b1f51
pkiop <pkiopb@gmail.com>
2022-01-20 17:55:05 +0900

    Button: 아이콘 예제 추가

--- playground/src/components/button.tsx
+++ playground/src/components/button.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { css } from '@emotion/react';
 import { Button as FCButton } from '../../../src';
+import LogoImg from '../icons/logo.svg';
+import CancelImg from '../icons/cancel.svg';
 
 const Button = () => {
   const [cnt, setCnt] = useState(0);
@@ -13,23 +15,59 @@ const Button = () => {
       >
         Button
       </h1>
-      <div
-        css={css`
-          .fc-button {
-            background-color: rgba(0, 0, 255, 0.8);
-            color: white;
-            &:disabled {
-              background-color: rgba(255, 0, 0, 0.3);
-            }
-          }
-        `}
-      >
+      <div>
         <div>cnt : {cnt}</div>
-        <FCButton onClick={() => setCnt((state) => state + 1)}>+</FCButton>
-        <FCButton onClick={() => setCnt((state) => state - 1)}>-</FCButton>
-        <FCButton onClick={() => setCnt((state) => state - 1)} disabled={true}>
-          -
-        </FCButton>
+        <div
+          css={css`
+            display: flex;
+            .fc-button {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              padding: 0.5rem 1rem;
+              background-color: rgba(0, 0, 255, 0.2);
+              color: black;
+              &:disabled {
+                background-color: rgba(255, 0, 0, 0.3);
+              }
+            }
+          `}
+        >
+          <FCButton onClick={() => setCnt((state) => state + 1)}>+</FCButton>
+          <FCButton onClick={() => setCnt((state) => state - 1)}>-</FCButton>
+          <FCButton onClick={() => setCnt((state) => state - 1)} disabled={true}>
+            -
+          </FCButton>
+        </div>
+        <div
+          css={css`
+            .fc-button {
+              display: flex;
+              align-items: center;
+              padding: 0.5rem 1rem;
+              background-color: rgba(0, 0, 255, 0.2);
+              color: black;
+              &:disabled {
+                background-color: rgba(255, 0, 0, 0.3);
+              }
+            }
+          `}
+        >
+          <FCButton onClick={() => setCnt(0)}>
+            취소하기
+            <img src={CancelImg} />
+          </FCButton>
+          <FCButton
+            css={css`
+              img {
+                margin-right: 1rem;
+              }
+            `}
+            onClick={() => (window.location.href = 'https://fastcampus.app')}
+          >
+            <img src={LogoImg} /> 패스트캠퍼스 기업강의로 이동
+          </FCButton>
+        </div>
       </div>
     </>
   );
