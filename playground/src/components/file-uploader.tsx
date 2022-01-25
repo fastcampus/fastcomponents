c015d44149d01a875ce9e1c1b098af7d61762b0f
pkiop <pkiopb@gmail.com>
2022-01-25 11:45:25 +0900

    file-uploader: dropzone 기능 추가

--- playground/src/components/file-uploader.tsx
+++ playground/src/components/file-uploader.tsx
@@ -1,37 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/react';
 import { FileUploader as FCFileUploader } from '../../../src';
 
 const FileUploader = () => {
+  const [fileList, setFileList] = useState<File[]>([]);
   const fileUploaderOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const path = e.target.value;
     console.log('path : ', path);
   };
+  const fileUploaderOnDropHandler = (files: File[]) => {
+    setFileList(files);
+  };
   return (
     <div>
-      <h1
+      <div
         css={css`
-          font-size: 4rem;
+          padding: 2rem;
         `}
       >
-        File Uploader
-      </h1>
-      <h3
+        <h1
+          css={css`
+            font-size: 4rem;
+          `}
+        >
+          File Uploader
+        </h1>
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          multiple : x, Accept : all
+        </h3>
+        <FCFileUploader input onChange={fileUploaderOnChangeHandler} />
+      </div>
+      <div
         css={css`
-          font-size: 2rem;
+          padding: 2rem;
         `}
       >
-        multiple : x, Accept : all
-      </h3>
-      <FCFileUploader onChange={fileUploaderOnChangeHandler} />
-      <h3
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          multiple : o, Accept : image/*
+        </h3>
+        <FCFileUploader input onChange={fileUploaderOnChangeHandler} multiple accept="image/*" />
+      </div>
+      <div
         css={css`
-          font-size: 2rem;
+          padding: 2rem;
         `}
       >
-        multiple : o, Accept : image/*
-      </h3>
-      <FCFileUploader onChange={fileUploaderOnChangeHandler} multiple accept="image/*" />
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          dropzone multiple Accept : image/*
+        </h3>
+        <div>
+          {fileList.map((e) => (
+            <div key={e.name}>{e.name}</div>
+          ))}
+        </div>
+
+        <FCFileUploader
+          css={css`
+            .dropzone {
+              width: 20rem;
+              height: 10rem;
+              background-color: rgba(0, 0, 0, 0.2);
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+          `}
+          onChange={fileUploaderOnChangeHandler}
+          onDrop={fileUploaderOnDropHandler}
+          multiple
+          accept="image/*"
+          dropzone
+          dropzoneChildren={<>Dropzone</>}
+          dropzoneActiveChildren={<>여기에 놓아주세요</>}
+        />
+      </div>
+      <div
+        css={css`
+          padding: 2rem;
+        `}
+      >
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          dropzone multiple x Accept : *
+        </h3>
+        <div>
+          {fileList.map((e) => (
+            <div key={e.name}>{e.name}</div>
+          ))}
+        </div>
+
+        <FCFileUploader
+          css={css`
+            .dropzone {
+              width: 20rem;
+              height: 10rem;
+              background-color: rgba(0, 0, 0, 0.2);
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+          `}
+          onChange={fileUploaderOnChangeHandler}
+          onDrop={fileUploaderOnDropHandler}
+          dropzone
+          dropzoneChildren={<>Dropzone</>}
+          dropzoneActiveChildren={<>여기에 놓아주세요</>}
+        />
+      </div>
     </div>
   );
 };
--- src/components/file-uploader/file-uploader.tsx
+++ src/components/file-uploader/file-uploader.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import type { FileUploaderProps } from '../../types/file-uploader.interface';
+import { useDropzone } from 'react-dropzone';
 
-const FileUploader = ({ className, onChange, multiple = false, accept = '' }: FileUploaderProps) => {
+const FileUploader = ({
+  className,
+  onChange,
+  onDrop,
+  multiple = false,
+  accept = '',
+  input = false,
+  dropzone = false,
+  dropzoneChildren,
+  dropzoneActiveChildren,
+}: FileUploaderProps) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept, multiple });
   return (
-    <input
-      className={`fc-file-uploader ${className}`}
-      type="file"
-      onChange={onChange}
-      multiple={multiple}
-      accept={accept}
-    ></input>
+    <div className={`fc-file-uploader ${className}`}>
+      {input && <input type="file" onChange={onChange} multiple={multiple} accept={accept}></input>}
+      {dropzone && (
+        <div className="dropzone" {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? dropzoneActiveChildren : dropzoneChildren}
+        </div>
+      )}
+    </div>
   );
 };
 
