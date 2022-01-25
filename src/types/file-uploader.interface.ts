5dbfdd1f7c25aeb79d5ab90278d8a067d3187d3e
pkiop <pkiopb@gmail.com>
2022-01-25 11:45:25 +0900

    file-uploader: dropzone 기능 추가

--- src/types/file-uploader.interface.ts
+++ src/types/file-uploader.interface.ts
@@ -3,6 +3,11 @@ import React from 'react';
 export interface FileUploaderProps {
   className?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onDrop?: (files: File[]) => void;
   multiple?: boolean;
   accept?: string;
+  input?: boolean;
+  dropzone?: boolean;
+  dropzoneChildren?: React.ReactNode;
+  dropzoneActiveChildren?: React.ReactNode;
 }
