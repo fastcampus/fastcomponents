62c06da84873ec17329073e99e84e18ffb034bb8
pkiop <pkiopb@gmail.com>
2022-01-26 14:19:30 +0900

    file-uploader: 리펙토링

--- src/components/file-uploader/dropzone.tsx
+++ src/components/file-uploader/dropzone.tsx
@@ -1,16 +1,34 @@
 import React, { useState, useCallback } from 'react';
 import { fromEvent } from 'file-selector';
 import type { DropzoneProps } from '../../types/file-uploader.interface';
+import { NO_DROP_CALLBACK, TO_MANY_FILES, HAVE_REJECTED_FILES } from './error';
 
-const TO_MANY_FILES = new Error('TO_MANY_FILES');
-class HAVE_REJECTED_FILES extends Error {
-  constructor(rejectedFiles: File[]) {
-    const newMessage = `올라가지 않은 파일이 있습니다. : ${rejectedFiles.map((file: File) => file.name)}`;
-    super(newMessage);
-    this.name = 'HAVE_REJECTED_FILES';
+const isFileAcceptable = (file: File, accept: string) => {
+  const [acceptType, acceptExtension] = accept.split('/'); // ex) image/jpg => ['image', 'jpg']
+  const [fileType] = file.type.split('/');
+  if (acceptExtension === '*') {
+    return acceptType === fileType;
   }
-}
-const NO_DROP_CALLBACK = new Error('NO_DROP_CALLBACK');
+  return accept === file.type;
+};
+
+const filterFiles = (droppedFiles: File[], accept: string | undefined) => {
+  const acceptedFiles: File[] = [];
+  const rejectedFiles: File[] = [];
+
+  droppedFiles.forEach((e: File) => {
+    if (!accept) {
+      return acceptedFiles.push(e);
+    }
+    if (isFileAcceptable(e, accept)) {
+      acceptedFiles.push(e);
+    } else {
+      rejectedFiles.push(e);
+    }
+    return;
+  });
+  return { acceptedFiles, rejectedFiles };
+};
 
 const Dropzone = ({ dropzoneActiveChildren, dropzoneChildren, onDrop, multiple, setError, accept }: DropzoneProps) => {
   const [isMouseHover, setIsMouseHover] = useState<boolean>(false);
@@ -18,58 +36,36 @@ const Dropzone = ({ dropzoneActiveChildren, dropzoneChildren, onDrop, multiple, 
   const onDropCb = async (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
+    setIsMouseHover(false);
 
     try {
       if (!onDrop) throw NO_DROP_CALLBACK;
-      const res = (await fromEvent(e as any)) as File[];
-      if (!multiple && res.length !== 1) {
+
+      const droppedFiles = (await fromEvent(e as any)) as File[];
+      if (!multiple && droppedFiles.length !== 1) {
         throw TO_MANY_FILES;
       }
-      const acceptedFiles: File[] = [];
-      const rejectedFiles: File[] = [];
-      res.forEach((e: File) => {
-        if (!accept) {
-          return acceptedFiles.push(e);
-        }
-        const typeSplit = accept.split('/');
-        if (typeSplit[1] === '*') {
-          if (typeSplit[0] === e.type.split('/')[0]) {
-            acceptedFiles.push(e);
-          } else {
-            rejectedFiles.push(e);
-          }
-          return;
-        }
-        if (accept === e.type) {
-          acceptedFiles.push(e);
-        } else {
-          rejectedFiles.push(e);
-        }
-        return;
-      });
 
-      setError && setError(null);
+      const { acceptedFiles, rejectedFiles } = filterFiles(droppedFiles, accept);
       onDrop(acceptedFiles);
+
       if (rejectedFiles.length !== 0) {
         throw new HAVE_REJECTED_FILES(rejectedFiles);
       }
+
+      setError && setError(null);
     } catch (err) {
       if (setError) {
         if (err === TO_MANY_FILES) {
-          console.error('너무 많은 파일을 드롭했습니다.');
           setError(TO_MANY_FILES);
         }
         if (err instanceof HAVE_REJECTED_FILES) {
-          console.log('do');
           setError(err);
         }
         if (err === NO_DROP_CALLBACK) {
           setError(NO_DROP_CALLBACK);
         }
-        console.log('error : ', err);
       }
-    } finally {
-      setIsMouseHover(false);
     }
   };
 
