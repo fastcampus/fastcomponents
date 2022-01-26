078f91cdb27ea6c543c06750130966664d6033b4
pkiop <pkiopb@gmail.com>
2022-01-26 14:35:14 +0900

    file-uploader: 파일 변경 함수를 하나로 통일

--- playground/src/components/file-uploader.tsx
+++ playground/src/components/file-uploader.tsx
@@ -5,17 +5,65 @@ import { FileUploader as FCFileUploader } from '../../../src';
 const FileUploader = () => {
   const [fileList, setFileList] = useState<File[]>([]);
   const [error, setError] = useState<Error | null>(null);
-  const fileUploaderOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const path = e.target.value;
-    console.log('path : ', path);
-  };
-
-  const fileUploaderOnDropHandler = (files: File[]) => {
+  const setFile = (files: File[]) => {
     setFileList(files);
   };
 
   return (
     <>
+      <div
+        css={css`
+          padding: 2rem;
+        `}
+      >
+        <h1
+          css={css`
+            font-size: 4rem;
+          `}
+        >
+          File Uploader
+        </h1>
+        <h2
+          css={css`
+            font-size: 2.6rem;
+          `}
+        >
+          선택된 파일들
+        </h2>
+        {fileList.map((file) => (
+          <div key={file.name}>{file.name}</div>
+        ))}
+        <div
+          css={css`
+            font-size: 1.8rem;
+            color: red;
+          `}
+        >
+          {error?.message}
+        </div>
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          multiple : x, Accept : all
+        </h3>
+        <FCFileUploader input setFile={setFile} />
+      </div>
+      <div
+        css={css`
+          padding: 2rem;
+        `}
+      >
+        <h3
+          css={css`
+            font-size: 2rem;
+          `}
+        >
+          multiple : o, Accept : image/*
+        </h3>
+        <FCFileUploader input setFile={setFile} multiple accept="image/*" />
+      </div>
       <div
         css={css`
           padding: 2rem;
@@ -28,17 +76,7 @@ const FileUploader = () => {
         >
           Dropzone-copy multiple X Accept : ''
         </h3>
-        {fileList.map((file) => (
-          <div key={file.name}>{file.name}</div>
-        ))}
-        <div
-          css={css`
-            font-size: 1.8rem;
-            color: red;
-          `}
-        >
-          {error?.message}
-        </div>
+
         <FCFileUploader
           css={css`
             .dropzone {
@@ -50,8 +88,7 @@ const FileUploader = () => {
               align-items: center;
             }
           `}
-          onChange={fileUploaderOnChangeHandler}
-          onDrop={fileUploaderOnDropHandler}
+          setFile={setFile}
           dropzone
           dropzoneChildren={<>Dropzone</>}
           dropzoneActiveChildren={<>여기에 놓아주세요</>}
@@ -70,17 +107,7 @@ const FileUploader = () => {
         >
           Dropzone-copy multiple O Accept : 'image/*'
         </h3>
-        {fileList.map((file) => (
-          <div key={file.name}>{file.name}</div>
-        ))}
-        <div
-          css={css`
-            font-size: 1.8rem;
-            color: red;
-          `}
-        >
-          {error?.message}
-        </div>
+
         <FCFileUploader
           css={css`
             .dropzone {
@@ -92,8 +119,7 @@ const FileUploader = () => {
               align-items: center;
             }
           `}
-          onChange={fileUploaderOnChangeHandler}
-          onDrop={fileUploaderOnDropHandler}
+          setFile={setFile}
           multiple
           accept="image/*"
           dropzone
