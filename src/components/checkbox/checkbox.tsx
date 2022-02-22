791ea331182e7a8fa810bcbf548e4af6594c6b5b
pkiop <pkiopb@gmail.com>
2022-02-22 18:03:13 +0900

    Fix: checkbox label 클릭 stopPropagation 추가

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -3,7 +3,10 @@ import type { CheckboxProps } from '../../types/checkbox.interface';
 
 const Checkbox = ({ checked, setChecked, disabled, className }: CheckboxProps) => {
   return (
-    <label className={`fc-checkbox ${className} ${checked ? 'checked' : ''} ${disabled ? 'disabled' : ''}`}>
+    <label
+      className={`fc-checkbox ${className} ${checked ? 'checked' : ''} ${disabled ? 'disabled' : ''}`}
+      onClick={(e) => e.stopPropagation()}
+    >
       <input type="checkbox" checked={checked} disabled={disabled} onChange={(e) => setChecked(e.target.checked)} />
     </label>
   );
