d45dfdd1f7f012cab51b83ab4b120c4c6eec172a
pkiop <pkiopb@gmail.com>
2022-02-24 11:53:24 +0900

    Feat: TextArea 컴포넌트 추가

---
+++ src/components/textarea/textarea.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import type { TextAreaProps } from '../../types/textarea.interface';
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    {
+      className,
+      defaultValue,
+      placeholder,
+      disabled,
+      onChange,
+      required,
+      value,
+      readOnly,
+      maxLength,
+      minLength,
+    }: TextAreaProps,
+    ref
+  ) => {
+    return (
+      <textarea
+        className={`fc-textarea ${className || ''}`}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        onChange={onChange}
+        ref={ref}
+        required={required}
+        value={value}
+        readOnly={readOnly}
+        maxLength={maxLength}
+        minLength={minLength}
+      ></textarea>
+    );
+  }
+);
+TextArea.displayName = 'FCTextArea';
+
+export default TextArea;
---
+++ src/types/textarea.interface.ts
@@ -0,0 +1,15 @@
+export interface TextAreaProps {
+  className?: string;
+  defaultValue?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
+  ref?: React.MutableRefObject<HTMLTextAreaElement>;
+  required?: boolean;
+  value?: string;
+  readOnly?: boolean;
+  max?: number;
+  maxLength?: number;
+  min?: number;
+  minLength?: number;
+}
