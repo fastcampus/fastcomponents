037d57c6ad14b3abdebb33e64f29bcb0a3013596
pkiop <pkiopb@gmail.com>
2022-03-14 17:11:19 +0900

    Feat: 파일업로더 초기 파일 입력값 추가

--- src/types/file-uploader.interface.ts
+++ src/types/file-uploader.interface.ts
@@ -3,17 +3,18 @@ import React from 'react';
 export type FileSize = number | string;
 
 export interface DropzoneProps {
+  accept?: string;
   dropzoneChildren?: React.ReactNode;
   dropzoneActiveChildren?: React.ReactNode;
-  setFiles: (files: File[]) => void;
-  multiple?: boolean;
-  accept?: string;
-  setError?: (err: Error | null) => void;
   fileMaxSize?: FileSize;
+  initialFiles?: File[];
+  multiple?: boolean;
+  setError?: (err: Error | null) => void;
+  setFiles: (files: File[]) => void;
 }
 export interface FileUploaderProps extends DropzoneProps {
   className?: string;
-  input?: boolean;
   dropzone?: boolean;
   fileUploadText?: string;
+  input?: boolean;
 }
