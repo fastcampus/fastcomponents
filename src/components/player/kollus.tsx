64cbf4fc87d05135b861e97d4ab43200875743cd
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 17:06:37 +0900

    vgController 타입 정의

--- src/components/player/kollus.tsx
+++ src/components/player/kollus.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 import { loadScript } from '@day1co/browser-util';
 import type { PlayerProps } from '../../types/player.interface';
+import type { VgController } from '../../global';
 
 const KOLLUS_SCRIPT_URL = 'https://file.kollus.com/vgcontroller/vg-controller-client.latest.min.js';
 
@@ -32,16 +33,16 @@ const KollusPlayer = ({
   onSeeked,
   ...restProps
 }: Omit<PlayerProps, 'vendor'>) => {
-  const iframeEl = useRef(null);
-  const [vgController, setVgController] = useState(null);
+  const iframeEl = useRef<HTMLIFrameElement | null>(null);
+  const [vgController, setVgController] = useState<VgController | null>(null);
 
   useEffect(() => {
     loadScript(KOLLUS_SCRIPT_URL).then(() => {
       if (onScriptLoaded) {
         onScriptLoaded();
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        setVgController(new window.VgControllerClient({ target_window: iframeEl.current.contentWindow }));
+        setVgController(
+          new window.VgControllerClient({ target_window: (iframeEl.current as HTMLIFrameElement).contentWindow })
+        );
       }
     });
   }, []);
@@ -50,69 +51,52 @@ const KollusPlayer = ({
     if (vgController) {
       console.log('vgController init');
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.LOADED, () => {
-        if (onLoaded) {
-          onLoaded();
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.READY, () => {
-        if (onReady) {
-          onReady();
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.PLAY, () => {
-        if (onPlay) {
-          onPlay();
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.PAUSE, () => {
-        if (onPause) {
-          onPause();
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.PROGRESS, (percent: number, position: number, duration: number) => {
-        if (onProgress) {
-          onProgress({ percent, position, duration });
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.DONE, () => {
-        if (onDone) {
-          onDone();
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.VOLUME_CHANGE, (volume) => {
-        if (onVolumeChanged) {
-          onVolumeChanged(volume);
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.SPEED_CHANGE, (speed) => {
-        if (onSpeedChanged) {
-          onSpeedChanged(speed);
-        }
-      });
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      vgController.on(VG_CONTROLLER_EVENT.SEEKED, () => {
-        if (onSeeked) {
-          onSeeked();
-        }
-      });
+      vgController
+        .on(VG_CONTROLLER_EVENT.LOADED, () => {
+          if (onLoaded) {
+            onLoaded();
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.READY, () => {
+          if (onReady) {
+            onReady();
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.PLAY, () => {
+          if (onPlay) {
+            onPlay();
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.PAUSE, () => {
+          if (onPause) {
+            onPause();
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.PROGRESS, (percent: number, position: number, duration: number) => {
+          if (onProgress) {
+            onProgress({ percent, position, duration });
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.DONE, () => {
+          if (onDone) {
+            onDone();
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.VOLUME_CHANGE, (volume: number) => {
+          if (onVolumeChanged) {
+            onVolumeChanged(volume);
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.SPEED_CHANGE, (speed: number) => {
+          if (onSpeedChanged) {
+            onSpeedChanged(speed);
+          }
+        })
+        .on(VG_CONTROLLER_EVENT.SEEKED, () => {
+          if (onSeeked) {
+            onSeeked();
+          }
+        });
     }
   }, [vgController]);
 
