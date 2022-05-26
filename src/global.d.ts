a40447cc16252aa7ad49a2cb0564ea808e49a682
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 17:06:37 +0900

    vgController 타입 정의

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,19 @@
 declare global {
   interface Window {
     VgControllerClient: {
-      new (vgControllerOption: VgControllerOption): void;
+      new (vgControllerOption: VgControllerOption): VgController;
     };
   }
 }
 
 export interface VgControllerOption {
-  target_window: HTMLElement;
+  target_window: any;
+}
+
+export interface VgController {
+  on(eventType: string, callback: () => void): this;
+  on(eventType: string, callback: (percent: number, position: number, duration: number) => void): this;
+  on(eventType: string, callback: (is_muted: boolean) => void): this;
+  on(eventType: string, callback: (volume: number) => void): this;
+  on(eventType: string, callback: (speed: number) => void): this;
 }
