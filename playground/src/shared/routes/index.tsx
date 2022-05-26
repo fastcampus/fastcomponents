ea02259c8254ee4add3079e1ae27bb8f9c4e057a
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 13:33:27 +0900

    playground에 player route 추가

--- playground/src/shared/routes/index.tsx
+++ playground/src/shared/routes/index.tsx
@@ -1,34 +1,10 @@
-// export const routes = [
-//   {
-//     path: '/account',
-//     name: 'account',
-//     component: AuthLayout,
-//     meta: { isPrivateRoute: false },
-//   //   children: [
-//   //     {
-//   //       path: '/account/sign-in',
-//   //       name: 'sign-in',
-//   //       component: SignIn,
-//   //     },
-//   //     {
-//   //       path: '/account/find-password',
-//   //       name: 'find-password',
-//   //       component: FindPassword,
-//   //     },
-//   //     {
-//   //       path: '/account/reset-password',
-//   //       name: 'reset-password',
-//   //       component: ResetPassword,
-//   //     },
-//   //   ],
-//   // },
-// ];
 import React from 'react';
 import type { RouteObject } from 'react-router-dom';
 import { useRoutes } from 'react-router-dom';
 import HomeLayout from '../../views/home/layout';
 import PaginationLayout from '../../views/pagination/layout';
 import CodeEditorLayout from '../../views/code-editor/layout';
+import PlayerLayout from '../../views/player/layout';
 
 export const routes: RouteObject[] = [
   {
@@ -56,6 +32,10 @@ export const routes: RouteObject[] = [
   {
     path: '/code-editor',
     element: <CodeEditorLayout />,
+  },
+  {
+    path: '/player',
+    element: <PlayerLayout />,
   },
 ];
 
