7b760a06b9950c88deb1d8c01350c92a4619fd4d
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 13:40:23 +0900

    playground에 player예제 생성

---
+++ playground/src/components/player.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import { Player as FCPlayer } from '../../../src';
+
+const Player = () => {
+  return (
+    <div css={css``}>
+      <FCPlayer className="test" />
+    </div>
+  );
+};
+
+export default Player;
