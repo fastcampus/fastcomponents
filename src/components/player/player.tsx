3533dbda478568e2c1233ef3e8ebcda29df410a4
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 13:56:05 +0900

    playerProps정의

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import type { PlayerProps } from '../../types/player.interface';
 
-const Player = ({ className, ...restProps }: { className: string }) => {
-  return <div className={`fc-player ${className}`} {...restProps} />;
+const Player = ({ vendor = 'KOLLUS', className, ...restProps }: PlayerProps) => {
+  return vendor === 'KOLLUS' ? (
+    <div className={`fc-player ${className}`} {...restProps}>
+      콜러스
+    </div>
+  ) : (
+    <div>다른 플레이어</div>
+  );
 };
 
 export default Player;
---
+++ src/types/player.interface.ts
@@ -0,0 +1,21 @@
+export interface PlayerProps {
+  src: string;
+  command: 'none' | 'play' | 'pause';
+  vendor?: 'KOLLUS' | 'YOUTUBE';
+  volume?: number;
+  speed?: number;
+  position?: number;
+  className?: string;
+  isIntroVideo?: boolean;
+  onScriptLoaded?: () => void;
+  onLoaded?: () => void;
+  onReady?: () => void;
+  onPlay?: () => void;
+  onPause?: () => void;
+  onProgress?: () => void;
+  onDone?: () => void;
+  onIntroVideoDone?: () => void;
+  onVolumeChanged?: () => void;
+  onSpeedChaned?: () => void;
+  onSeeked?: () => void;
+}
