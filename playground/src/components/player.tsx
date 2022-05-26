f12bdbc15a512f99fab41b516308a8ec9734e73f
SuJin <sujin_park@day1company.co.kr>
2022-05-26 16:45:27 +0900

    feat: playground에 적용

--- playground/src/components/player.tsx
+++ playground/src/components/player.tsx
@@ -29,10 +29,12 @@ const Player = () => {
   const onIntroVideoDone = () => {
     console.log('onIntroVideoDone');
   };
-  const onVolumeChanged = () => {
+  const onVolumeChanged = (volume: number) => {
+    console.log(volume);
     console.log('onVolumeChanged');
   };
-  const onSpeedChanged = () => {
+  const onSpeedChanged = (speed: number) => {
+    console.log(speed);
     console.log('onSpeedChaned');
   };
   const onSeeked = () => {
@@ -40,10 +42,19 @@ const Player = () => {
   };
 
   return (
-    <div css={css``}>
+    <div
+      css={css`
+        .kollus-player {
+          display: inline-block;
+          width: 1200px;
+          height: 800px;
+        }
+      `}
+    >
       <FCPlayer
         src="https://v.kr.kollus.com/s?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdWlkIjoic3VqaW5fcGFyay0xNzQwMjUiLCJleHB0IjoxNjUzNTkwNzQ2LCJtYyI6W3sibWNrZXkiOiJzSTB4RlVEQyIsIm1jcGYiOiJmYXN0Y2FtcC1wYzEtaGQtMSJ9XX0.5LVVqQt3F3LX1O_dR_a9UfJUlKgL0eSXEhKI2qSeRc0&custom_key=0b50155632326ca34e42550f10d6fca2c42c7b62a47eee4841aabd0cd1a913ea&s=0"
         command="none"
+        className="kollus-player"
         onScriptLoaded={onScriptLoaded}
         onLoaded={onLoaded}
         onReady={onReady}
