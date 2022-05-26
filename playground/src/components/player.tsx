93e4de2a7557b4238b833daaaf0ab50709a28f7b
Jaewon-Noh <jwno@day1company.co.kr>
2022-05-26 13:56:16 +0900

    props적용

--- playground/src/components/player.tsx
+++ playground/src/components/player.tsx
@@ -5,7 +5,10 @@ import { Player as FCPlayer } from '../../../src';
 const Player = () => {
   return (
     <div css={css``}>
-      <FCPlayer className="test" />
+      <FCPlayer
+        src="https://v.kr.kollus.com/s?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdWlkIjoianduby00NTQ3NiIsImV4cHQiOjE2NTA5MTQ4MzcsIm1jIjpbeyJtY2tleSI6InZPWXhmVGVrIiwibWNwZiI6ImZhc3RjYW1wLXBjMS1oaWdoIn1dfQ.QLO7pjuoc21l1JOnBm0I4hYNdjgOSEn5CFL9Gsx_WxI&custom_key=0b50155632326ca34e42550f10d6fca2c42c7b62a47eee4841aabd0cd1a913ea&s=0"
+        command="none"
+      />
     </div>
   );
 };
