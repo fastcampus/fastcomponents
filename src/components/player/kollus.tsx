571a17f13666d16d9c519776e74dcc6c5e344558
SuJin <sujin_park@day1company.co.kr>
2022-05-26 16:08:08 +0900

    feat: kollus event 정의 및 사용처에서 전달한 함수 실행하도록 적용

--- src/components/player/kollus.tsx
+++ src/components/player/kollus.tsx
@@ -4,7 +4,33 @@ import type { PlayerProps } from '../../types/player.interface';
 
 const KOLLUS_SCRIPT_URL = 'https://file.kollus.com/vgcontroller/vg-controller-client.latest.min.js';
 
-const KollusPlayer = ({ className, src, onScriptLoaded, ...restProps }: Omit<PlayerProps, 'vendor'>) => {
+const VG_CONTROLLER_EVENT = {
+  PLAY: 'play',
+  PAUSE: 'pause',
+  LOADED: 'loaded',
+  READY: 'ready',
+  PROGRESS: 'progress',
+  DONE: 'done',
+  VOLUME_CHANGE: 'volumechange',
+  SPEED_CHANGE: 'speedchange',
+  SEEKED: 'seeked',
+};
+
+const KollusPlayer = ({
+  className,
+  src,
+  onScriptLoaded,
+  onLoaded,
+  onReady,
+  onPlay,
+  onPause,
+  onProgress,
+  onDone,
+  onVolumeChanged,
+  onSpeedChanged,
+  onSeeked,
+  ...restProps
+}: Omit<PlayerProps, 'vendor'>) => {
   const iframeEl = useRef(null);
   const [vgController, setVgController] = useState(null);
 
@@ -14,15 +40,84 @@ const KollusPlayer = ({ className, src, onScriptLoaded, ...restProps }: Omit<Pla
         onScriptLoaded();
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        setVgController(new window.VgControllerClient({ target_window: iframeEl.current }));
-        // console.log(iframeEl.current);
+        setVgController(new window.VgControllerClient({ target_window: iframeEl.current.contentWindow }));
       }
     });
   }, []);
 
+  useEffect(() => {
+    if (vgController) {
+      console.log('vgController init');
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.LOADED, () => {
+        if (onLoaded) {
+          onLoaded();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.READY, () => {
+        if (onReady) {
+          onReady();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.PLAY, () => {
+        if (onPlay) {
+          onPlay();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.PAUSE, () => {
+        if (onPause) {
+          onPause();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.PROGRESS, () => {
+        if (onProgress) {
+          onProgress();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.DONE, () => {
+        if (onDone) {
+          onDone();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.VOLUME_CHANGE, () => {
+        if (onVolumeChanged) {
+          onVolumeChanged();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.SPEED_CHANGE, () => {
+        if (onSpeedChanged) {
+          onSpeedChanged();
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      vgController.on(VG_CONTROLLER_EVENT.SEEKED, () => {
+        if (onSeeked) {
+          onSeeked();
+        }
+      });
+    }
+  }, [vgController]);
+
   return (
     <div className={`fc-player ${className}`} {...restProps}>
-      <iframe ref={iframeEl} />
+      <iframe src={src} ref={iframeEl} allow="autoplay" allowFullScreen />
     </div>
   );
 };
