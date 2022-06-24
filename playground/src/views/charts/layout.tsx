ccfecd6d7f18ac2274f7c583cdf740baf84a3450
Jaewon-Noh <jwno@day1company.co.kr>
2022-06-24 16:29:30 +0900

    column chart 예제 생성

--- playground/src/views/charts/layout.tsx
+++ playground/src/views/charts/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { PieChart, BarChart } from '../../../../src';
-import type { PieChartData, BarChartData } from '../../../../src/types/charts.interface';
+import { PieChart, BarChart, ColumnChart } from '../../../../src';
+import type { PieChartData, BarChartData, ColumnChartData } from '../../../../src';
 
 const ChartBlock = styled.section`
   width: 60rem;
@@ -70,6 +70,37 @@ const ChartLayout = () => {
       value: 4,
     },
   ];
+
+  const ColumnChartData: ColumnChartData[] = [
+    {
+      label: '월요일',
+      value: 26,
+    },
+    {
+      label: '화요일',
+      value: 24,
+    },
+    {
+      label: '수요일',
+      value: 17,
+    },
+    {
+      label: '목요일',
+      value: 14,
+    },
+    {
+      label: '금요일',
+      value: 9,
+    },
+    {
+      label: '토요일',
+      value: 6,
+    },
+    {
+      label: '일요일',
+      value: 4,
+    },
+  ];
   return (
     <section>
       <span>Pie chart</span>
@@ -80,6 +111,11 @@ const ChartLayout = () => {
       <span>Bar chart</span>
       <ChartBlock>
         <BarChart data={BarChartData} />
+      </ChartBlock>
+
+      <span>Column chart</span>
+      <ChartBlock>
+        <ColumnChart data={ColumnChartData} />
       </ChartBlock>
     </section>
   );
