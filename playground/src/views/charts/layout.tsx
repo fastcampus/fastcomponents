0edd43345ca569ff419ecf6a7f75d5356ff7495f
Jaewon-Noh <jwno@day1company.co.kr>
2022-06-24 16:20:02 +0900

    bar chart 예제 생성

--- playground/src/views/charts/layout.tsx
+++ playground/src/views/charts/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { PieChart } from '../../../../src';
-import type { PieChartData } from '../../../../src/types/charts.interface';
+import { PieChart, BarChart } from '../../../../src';
+import type { PieChartData, BarChartData } from '../../../../src/types/charts.interface';
 
 const ChartBlock = styled.section`
   width: 60rem;
@@ -9,7 +9,38 @@ const ChartBlock = styled.section`
 `;
 
 const ChartLayout = () => {
-  const data: PieChartData[] = [
+  const PieChartData: PieChartData[] = [
+    {
+      label: '월요일',
+      value: 26,
+    },
+    {
+      label: '화요일',
+      value: 24,
+    },
+    {
+      label: '수요일',
+      value: 17,
+    },
+    {
+      label: '목요일',
+      value: 14,
+    },
+    {
+      label: '금요일',
+      value: 9,
+    },
+    {
+      label: '토요일',
+      value: 6,
+    },
+    {
+      label: '일요일',
+      value: 4,
+    },
+  ];
+
+  const BarChartData: BarChartData[] = [
     {
       label: '월요일',
       value: 26,
@@ -41,8 +72,14 @@ const ChartLayout = () => {
   ];
   return (
     <section>
+      <span>Pie chart</span>
       <ChartBlock>
-        <PieChart data={data} />
+        <PieChart data={PieChartData} />
+      </ChartBlock>
+
+      <span>Bar chart</span>
+      <ChartBlock>
+        <BarChart data={BarChartData} />
       </ChartBlock>
     </section>
   );
